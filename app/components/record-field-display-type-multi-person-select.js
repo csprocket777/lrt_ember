92f99ed94cd166c4f8ddfb3dfcadb79ea9796494
Chuck Shipman <chshipma@cisco.com>
2014-03-22 09:45:59 -0500

    Fixed 'unsavedRecords' functionality and started to add the "Personnel" option type for record form fields.

---
+++ app/components/record-field-display-type-multi-person-select.js
@@ -0,0 +1,8 @@
+/**
+ * Created by chshipma on 3/14/14.
+ */
+
+import recordFieldDisplayTypeBase from "appkit/components/record-field-display-type-base";
+
+export default recordFieldDisplayTypeBase.extend({
+});
