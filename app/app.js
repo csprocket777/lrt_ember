08e81454229ac4c44163c2f1fe52b3c0d6c1768b
Chuck Shipman <chshipma@cisco.com>
2014-03-31 16:19:27 -0500

    A fix for the ember-resolver / ember-inspector->data panel issue

--- app/app.js
+++ app/app.js
@@ -59,18 +59,18 @@ Ember.Handlebars.helper('related-record-display-type', relatedRecordDisplayTypeH
 Ember.Handlebars.helper('extended-link-to', extendedLinkTo);
 Ember.Handlebars.registerHelper('trigger-event', trigger);
 
-DS.DebugAdapter.reopen({
-    getModelTypes: function() {
-        var self = this;
-        return Ember.keys(requirejs._eak_seen).filter(function(key) {
-            return !!key.match(/^appkit\/models\//) && self.detect(require(key).default);
-        }).map(function(key){
-                var type = require(key).default, typeKey = key.match(/^appkit\/models\/(.*)/)[1];
-                type.toString = function() { return typeKey; };
-                return type;
-            });
-    }
-});
+//DS.DebugAdapter.reopen({
+//    getModelTypes: function() {
+//        var self = this;
+//        return Ember.keys(requirejs._eak_seen).filter(function(key) {
+//            return !!key.match(/^appkit\/models\//) && self.detect(require(key).default);
+//        }).map(function(key){
+//                var type = require(key).default, typeKey = key.match(/^appkit\/models\/(.*)/)[1];
+//                type.toString = function() { return typeKey; };
+//                return type;
+//            });
+//    }
+//});
 
 
 // THIS IS A CUSTOM DS MODEL ATTRIBUTE TYPE FUNCTION
